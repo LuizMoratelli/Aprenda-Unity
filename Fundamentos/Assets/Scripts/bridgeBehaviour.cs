d0a595263e5072cc8e025c62c934e5720f2c8503
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2018-09-07 19:45:21 -0300

    Otimizado a instância de novas pontes

--- Fundamentos/Assets/Scripts/bridgeBehaviour.cs
+++ Fundamentos/Assets/Scripts/bridgeBehaviour.cs
@@ -7,6 +7,8 @@ public class bridgeBehaviour : MonoBehaviour {
     private gameController _gameController;
     private Rigidbody2D _rigidBody;
 
+    private bool instantiated;
+
 	void Start () {
         _rigidBody = GetComponent<Rigidbody2D>();
 		_gameController = FindObjectOfType(typeof(gameController)) as gameController;
@@ -20,6 +22,18 @@ public class bridgeBehaviour : MonoBehaviour {
         // Destrói o objeto quando ele sair da cena
         if (transform.position.x < _gameController.destructionDistance.position.x) {
             Destroy(this.gameObject);
+        }
+
+        // Instancia a nova ponte caso exista apenas uma ponte
+        if (!instantiated) {
+            if (transform.position.x < _gameController.bridge.transform.position.x) {
+                instantiated = true;
+                GameObject newBridge = Instantiate(_gameController.bridge); // Instancia a nova ponte
+                // Define a posiação da nova ponte como a posição da ponte atual mais a largura do prefab da ponte
+                newBridge.transform.position = new Vector2 (transform.position.x + _gameController.BridgeWidth, transform.position.y);
+                // Define que a nova ponte será filha da Área jogável
+                newBridge.transform.SetParent(_gameController.playableArea.transform);
+            }
         }
 	}
 }
