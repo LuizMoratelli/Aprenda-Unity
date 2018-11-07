b8b4727797aa78a7269a41eee77c9b575a1de0e0
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2018-11-07 00:18:41 -0200

    Bunny -> Pulo

---
+++ Fundamentos/Assets/Scripts/bunnyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bunnyController : MonoBehaviour {
+
+    [Header("Componentes do Objeto")]
+    private Rigidbody2D _rigidbody;
+
+    [Header("Atributos do Objeto")]
+    public float jumpForce;
+
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("Jump")) { 
+            _rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+	}
+}
