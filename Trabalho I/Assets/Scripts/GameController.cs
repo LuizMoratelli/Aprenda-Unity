76bb59c758db52aaff8409293eb93d6a48bfa420
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2018-11-17 09:17:26 -0200

    Plataforma OnWay

--- Trabalho I/Assets/Scripts/GameController.cs
+++ Trabalho I/Assets/Scripts/GameController.cs
@@ -7,12 +7,14 @@ public class GameController : MonoBehaviour {
     [Header("Global Variables")]
     public float terrainSpeed;
     public GameObject[] _terrainPrefabs;
+    public PlayerController _player;
+    public float _playerPosY;
 
 	void Start () {
-		
+		_player = FindObjectOfType(typeof(PlayerController)) as PlayerController;
 	}
 	
 	void Update () {
-		
+		_playerPosY = _player.transform.position.y;
 	}
 }
---
+++ Trabalho I/Assets/Scripts/OnWay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnWay : MonoBehaviour {
+
+    public Transform collisionHeight;
+    private GameController _gameController;
+    private BoxCollider2D _collider;
+
+	void Start () {
+		_gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        _collider = GetComponent<BoxCollider2D>();
+	}
+	
+	void Update () {
+		if (_gameController._playerPosY > collisionHeight.transform.position.y) {
+            if (Input.GetKey(KeyCode.S)) {
+                _collider.enabled = false;
+            } else {
+                _collider.enabled = true;
+            }
+        } else {
+            _collider.enabled = false;
+        }
+	}
+}
