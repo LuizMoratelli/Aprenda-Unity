1ac070f6e72bda6f9737cd5a156ebc8d6042ea8e
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2019-04-14 01:10:42 -0300

    Sistema inicial de combate

--- CardPrototype/Assets/Scripts/TurnManager.cs
+++ CardPrototype/Assets/Scripts/TurnManager.cs
@@ -10,6 +10,48 @@ public class TurnManager : MonoBehaviour
     public Card playerCard;
     public GameObject waitingCard;
 
+    private const float MAX_CHANCE_TO_ATTACK = 0.7f;
+
+    private const float BASE_CHANCE_TO_HIT_EVENT = 0.7f;
+    private const float MAX_CHANCE_TO_HIT_EVENT = 0.95f;
+    private const float BASE_CHANCE_TO_DEFEND_EVENT = 0.7f;
+    private const float MAX_CHANCE_TO_DEFEND_EVENT = 0.95f;
+
+    private const float BASE_CHANCE_TO_HIT_PLAYER = 0.5f;
+    private const float MAX_CHANCE_TO_HIT_PLAYER = 0.90f;
+    private const float BASE_CHANCE_TO_DEFEND_PLAYER = 0.5f;
+    private const float MAX_CHANCE_TO_DEFEND_PLAYER = 0.90f;
+
+    private bool playerHitEvent {
+        get {
+            return chanceToHitEvent() > getChance();
+        }
+    }
+
+    private bool eventHitPlayer {
+        get {
+            return chanceToHitPlayer() > getChance();
+        }
+    }
+
+    private bool playerDefendEvent {
+        get {
+            return chanceToDefendEvent() > getChance();
+        }
+    }
+
+    private bool eventDefendPlayer {
+        get {
+            return chanceToDefendPlayer() > getChance();
+        }
+    }
+
+    private bool eventActionisAttack {
+        get {
+            return chanceToEventAttack() > getChance();
+        }
+    }
+
     public void Start()
     {
         player.SetCard(playerCard);
@@ -18,21 +60,33 @@ public class TurnManager : MonoBehaviour
 
     public void Attack()
     {
-        player.Attack(cardEvent);
-
-        if (cardEvent.defense <= 0)
+        if (eventActionisAttack)
         {
-            WaitNewEvent();
+            if (playerHitEvent) player.Attack(cardEvent);
+            if (eventHitPlayer) cardEvent.Attack(player);
+            else Debug.Log("Inimigo errou");
         }
         else
         {
-            cardEvent.Attack(player);
+            if (playerHitEvent && !eventDefendPlayer) player.Attack(cardEvent);
+            else Debug.Log("Errou!!!");
         }
+
+        if (cardEvent.defense <= 0) WaitNewEvent();
     }
 
     public void Defend()
     {
+        if (eventActionisAttack)
+        {
+            if (eventHitPlayer && !playerDefendEvent) cardEvent.Attack(player);
+            else Debug.Log("Player Defendeu");
+        }
+    }
 
+    public void Explore()
+    {
+        GenerateNewEvent();
     }
 
     public void Escape()
@@ -40,14 +94,49 @@ public class TurnManager : MonoBehaviour
 
     }
 
+    private float getChance()
+    {
+        return Random.Range(0f, 1f);
+    }
+
+    private float chanceToEventAttack()
+    {
+        float chance = (float)cardEvent.defense / (cardEvent.defense + cardEvent.attack);
+
+        return chance < MAX_CHANCE_TO_ATTACK ? chance : MAX_CHANCE_TO_ATTACK;
+    }
+
+    private float chanceToDefendPlayer()
+    {
+        float chance = BASE_CHANCE_TO_DEFEND_PLAYER + (cardEvent.level - player.level) * 5;
+
+        return chance < MAX_CHANCE_TO_DEFEND_PLAYER ? chance : MAX_CHANCE_TO_HIT_PLAYER;
+    }
+
+    private float chanceToDefendEvent()
+    {
+        float chance = BASE_CHANCE_TO_DEFEND_EVENT + (player.level - cardEvent.level) * 5;
+
+        return chance < MAX_CHANCE_TO_DEFEND_EVENT ? chance : MAX_CHANCE_TO_DEFEND_EVENT;
+    }
+
+    private float chanceToHitPlayer()
+    {
+        float chance = BASE_CHANCE_TO_HIT_PLAYER + (cardEvent.level - player.level) * 5;
+
+        return chance < MAX_CHANCE_TO_HIT_PLAYER ? chance : MAX_CHANCE_TO_HIT_PLAYER;
+    }
+
+    private float chanceToHitEvent()
+    {
+        float chance = BASE_CHANCE_TO_HIT_EVENT + (player.level - cardEvent.level) * 5;
+
+        return chance < MAX_CHANCE_TO_HIT_EVENT ? chance : MAX_CHANCE_TO_HIT_EVENT;
+    }
+
     public void WaitNewEvent()
     {
         waitingCard.SetActive(true);
-    }
-
-    public void Explore()
-    {
-        GenerateNewEvent();
     }
 
     private void GenerateNewEvent()
